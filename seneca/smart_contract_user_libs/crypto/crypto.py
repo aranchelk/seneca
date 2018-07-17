8cc7c49439d17d238761d34303102c006ae95ad5
Stuart Farmer <stuart@lamden.io>
2018-07-16 18:25:17 -0700

    hashing added to seneca

--- seneca/smart_contract_user_libs/crypto/crypto.py
+++ seneca/smart_contract_user_libs/crypto/crypto.py
@@ -1,9 +1,28 @@
 import hashlib
+from functools import partial
+
+'''
+    Highly abstracted hashing library that exposes the algorithms guaranteed by hashlib a la Python 3.
+    These functions are the following: blake2b, shake_128, sha3_384, sha3_224, shake_256, md5, sha256, 
+    sha512, sha1, sha224, sha3_256, blake2s, sha3_512, sha384
+'''
 
 
-def sha256(d: bytes, hex=False):
+def hash_data(d: bytes, algo: str, as_hex=False):
+    assert algo in hashlib.algorithms_guaranteed
     assert type(d) == bytes, 'd must be of type bytes.'
-    m = hashlib.sha256()
-    m.update(d)
-    return m.hexdigest() if hex else m.digest()
 
+    m = hashlib.new(algo)
+    m.update(d)
+    return m.hexdigest() if as_hex else m.digest()
+
+
+def f(algo: str):
+    assert algo in hashlib.algorithms_guaranteed
+
+    return partial(hash_data, algo=algo)
+
+
+exports = {
+    key: f(key) for key in hashlib.algorithms_guaranteed
+}
