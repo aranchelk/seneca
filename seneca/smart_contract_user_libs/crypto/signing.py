e4ed184bfd7c7a82d12b1dd51de92acbf55412f4
Stuart Farmer <stuart@lamden.io>
2018-07-18 09:57:46 -0700

    slimmer signing

--- seneca/smart_contract_user_libs/crypto/signing.py
+++ seneca/smart_contract_user_libs/crypto/signing.py
@@ -3,14 +3,10 @@ import nacl.encoding
 import nacl.signing
 
 
-def verify(v: str, msg, sig):
-    v = bytes.fromhex(v)
-    sig = bytes.fromhex(sig)
+def verify(v: bytes, msg: bytes, sig: bytes):
     v = nacl.signing.VerifyKey(v)
     try:
         v.verify(msg, sig)
     except nacl.exceptions.BadSignatureError:
-        return False
-    except Exception:
         return False
     return True
